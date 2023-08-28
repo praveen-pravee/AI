a90d5a006abcf89c959414fd1fcaa32eab394883
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-08-28 12:53:04 +0530

    wikipedia dot py

--- AI-Assistant/wikipedia.py
+++ AI-Assistant/wikipedia.py
@@ -1,8 +1,14 @@
 # Wiki
 import wikipedia
 
+search = str(input("Wiki : "))
+result = wikipedia.summary(query, sentences = 3)
+print(result)
+
+"""
+It's works butI got some errors 
 search = wikipedia.search('')
 page = wikipedia.page('')
 summary = wikipedia.summary('')
-
-# reference, title
+reference, title
+"""
