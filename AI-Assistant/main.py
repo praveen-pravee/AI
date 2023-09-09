801f121daaca6e563298f24899306a3d7c0e157d
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-09-09 16:53:10 +0530

    Update main.py

--- AI-Assistant/main.py
+++ AI-Assistant/main.py
@@ -1,10 +1,13 @@
 import pyttsx3
 import speech_recognition as sr
+import wikipedia
+import datetime
+import webbrowser
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.serProperty('rate', 140)
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 145)
+engine.setProperty('voice', voices[1].id)
 
 def speak(audio):
     engine.say(audio)
@@ -18,26 +21,57 @@ def commands():
         print("Speak Any...")
         audio = rObj.listen(source)
     try:
-        print("Processing Sir...")
+        print("One Sec...")
         query=rObj.recognize_google(audio, language='en-in')
         print(f"You Just said : {query}\n")
     except:
         print("Sorry Try Again..")
         speak("please tell me again sir")
-        return None
+        query= 'None'
     return query
-    
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     while True:
+
         query = commands().lower()
-        
-        if 'who are you' in query:
-            speak("I'm a Assistant for you")
-            
-        if 'computer' in query:
-            speak('yes I am a computer')
-            
-        if 'bye' in query:
-            speak('bye sir, you can call me anytime!)
+
+        if 'sleep' in query:
+            speak("Bye sir, you can call me any time sir.")
+            print("Bye sir, you can call me any time sir.")
             break
+
+        if 'search' in query:
+            speak("Searching in Internet...")
+            query = query.replace("the internet", "")
+            query = wikipedia.search(query)
+            result = wikipedia.summary(query, sentences=2)
+            print(result)
+            speak(result)
+
+        if 'time' in query:
+            time_var = datetime.datetime.now().strftime("%I:%M:%S")
+            speak(f"Sir,the Time is {time_var}")
+
+        if 'day' in query:
+            day = datetime.datetime.now().strftime("%A")
+            speak(f"today is {day} Sir")
+
+        if 'date' in query:
+            date = datetime.datetime.now().strftime("%A:%B:%Y")
+            speak(f"today date is {date} Sir")
+
+        if 'open firefox' in query:
+            speak("opening firefox sir")
+            firefox = "C:/Program Files/Mozilla Firefox/firefox.exe %s"
+            webbrowser.get(firefox).open_new("https://google.com")
+
+        if 'open browser' in query:
+            speak("opening microsoft edge sir")
+            edge = "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe %s"
+            webbrowser.get(edge).open_new("https://google.com")
+
+        if 'open youtube' in query:
+            speak('opening youtube sir')
+            edge = "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe %s"
+            webbrowser.get(edge).open_new("https://youtube.com")
+
