5930c0804b72d03f773bdc4818070622d40d3f4d
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-09-09 19:51:36 +0530

    Update main.py

--- AI-Assistant/main.py
+++ AI-Assistant/main.py
@@ -1,8 +1,11 @@
+import time
 import pyttsx3
 import speech_recognition as sr
 import wikipedia
 import datetime
 import webbrowser
+import pyautogui
+import os
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -35,18 +38,27 @@ if __name__ == "__main__":
 
         query = commands().lower()
 
+        if 'still wait' in query:
+            speak("ok sir, I'm waiting")
+            time.sleep(5)
+            speak("I'm Ready waitting your command sir")
+
         if 'sleep' in query:
             speak("Bye sir, you can call me any time sir.")
             print("Bye sir, you can call me any time sir.")
             break
 
         if 'search' in query:
-            speak("Searching in Internet...")
-            query = query.replace("the internet", "")
-            query = wikipedia.search(query)
-            result = wikipedia.summary(query, sentences=2)
-            print(result)
-            speak(result)
+            speak("Searching Internet...")
+            try:
+                query = query.replace("the internet", "")
+                query = wikipedia.search(query)
+                result = wikipedia.summary(query, sentences=2)
+                print(result)
+                speak(result)
+            except:
+                speak("I don't know sir, sorry maybe I can't recognize your voice sorry try agin sir")
+                print("Try Again")
 
         if 'time' in query:
             time_var = datetime.datetime.now().strftime("%I:%M:%S")
@@ -75,3 +87,19 @@ if __name__ == "__main__":
             edge = "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe %s"
             webbrowser.get(edge).open_new("https://youtube.com")
 
+        if 'open start' in query:
+            pyautogui.moveTo(0,475, 1)
+            pyautogui.moveTo(74,18, 1)
+            pyautogui.click()
+
+        if 'open notepad' in query:
+            speak("opening notepad for you sir")
+            app_path="C:/Program Files/Sublime Text/sublime_text.exe"
+            os.startfile(app_path)
+            time.sleep(9)
+            pyautogui.moveTo(20,34,1)
+            pyautogui.click()
+            pyautogui.moveTo(35,53,1)
+            pyautogui.click()
+            
+            
