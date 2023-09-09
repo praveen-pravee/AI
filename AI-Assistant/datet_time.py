5debdb21775665d9c9c16b1f16c72729c93f84cb
praveen <128697412+Thekid-pveen@users.noreply.github.com>
2023-09-09 19:58:53 +0530

    datetime py

---
+++ AI-Assistant/datet_time.py
@@ -0,0 +1,13 @@
+import datetime
+
+time = datetime.datetime.now().strftime("%I,%M,%S")
+
+hour = datetime.datetime.now().strftime("%I")
+
+day = datetime.datetime.now().strftime("%A")
+
+month = datetime.datetime.now().strftime("%B")
+
+date = datetime.datetime.now().strftime("%d")
+
+print("which you want")
